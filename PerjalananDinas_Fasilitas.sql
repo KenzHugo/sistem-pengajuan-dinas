65888158bac58c4cc934eb48de1468516977858d
KenzHugo <kenziehugo20@gmail.com>
2026-08-28 10:05:43 +0700

    Menambahkan Datanya Sql Data Dami

---
+++ PerjalananDinas_Fasilitas.sql
@@ -0,0 +1,8 @@
+INSERT INTO [PerjalananDinas_Fasilitas] ([Kriteria], [HakTransport], [HakUangMakan], [HakUangSaku], [HakTiket], [HakHotel]) VALUES ('1. < 200 KM & < 8 Jam', 1, 0, 0, 0, 0);
+GO
+INSERT INTO [PerjalananDinas_Fasilitas] ([Kriteria], [HakTransport], [HakUangMakan], [HakUangSaku], [HakTiket], [HakHotel]) VALUES ('2. < 200 KM & 8 - 12 Jam', 1, 1, 0, 0, 0);
+GO
+INSERT INTO [PerjalananDinas_Fasilitas] ([Kriteria], [HakTransport], [HakUangMakan], [HakUangSaku], [HakTiket], [HakHotel]) VALUES ('3. > 200 KM atau  >12 Jam', 1, 1, 1, 0, 0);
+GO
+INSERT INTO [PerjalananDinas_Fasilitas] ([Kriteria], [HakTransport], [HakUangMakan], [HakUangSaku], [HakTiket], [HakHotel]) VALUES ('4. Menginap', 1, 1, 1, 1, 1);
+GO
---
+++ PerjalananDinas_Tarif.sql
@@ -0,0 +1,60 @@
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Amerika / Singapura', 1.0000, 'Operator - Karu', 25.0000, 40.0000, 15.0000, 0.2000, 85.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Amerika / Singapura', 2.0000, 'Assup - Supervisor', 25.0000, 45.0000, 20.0000, 0.2000, 100.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Amerika / Singapura', 3.0000, 'Assman - Manager', 40.0000, 50.0000, 30.0000, 0.2000, 115.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Amerika / Singapura', 4.0000, 'GM - Wadir', 50.0000, 60.0000, 40.0000, 0.2000, 160.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Amerika / Singapura', 5.0000, 'Direktur - Dirut', 90.0000, 100.0000, 85.0000, 0.2000, 0.0010, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Asia', 1.0000, 'Operator - Karu', 20.0000, 20.0000, 12.0000, 0.2000, 70.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Asia', 2.0000, 'Assup - Supervisor', 25.0000, 30.0000, 15.0000, 0.2000, 85.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Asia', 3.0000, 'Assman - Manager', 35.0000, 40.0000, 25.0000, 0.2000, 105.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Asia', 4.0000, 'GM - Wadir', 50.0000, 45.0000, 40.0000, 0.2000, 145.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Asia', 5.0000, 'Direktur - Dirut', 60.0000, 75.0000, 75.0000, 0.2000, 0.0010, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Eropa', 1.0000, 'Operator - Karu', 25.0000, 40.0000, 15.0000, 0.2000, 100.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Eropa', 2.0000, 'Assup - Supervisor', 25.0000, 55.0000, 30.0000, 0.2000, 115.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Eropa', 3.0000, 'Assman - Manager', 30.0000, 60.0000, 30.0000, 0.2000, 145.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Eropa', 4.0000, 'GM - Wadir', 50.0000, 70.0000, 70.0000, 0.2000, 225.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Eropa', 5.0000, 'Direktur - Dirut', 90.0000, 120.0000, 110.0000, 0.2000, 0.0010, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jakarta / Luar Jawa', 1.0000, 'Operator - Karu', 0.0100, 100000.0000, 75000.0000, 0.1000, 600000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jakarta / Luar Jawa', 2.0000, 'Assup - Supervisor', 0.0100, 150000.0000, 100000.0000, 0.1000, 600000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jakarta / Luar Jawa', 3.0000, 'Assman - Manager', 0.0100, 175000.0000, 125000.0000, 0.1000, 750000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jakarta / Luar Jawa', 4.0000, 'GM - Wadir', 0.0100, 225000.0000, 150000.0000, 0.1000, 900000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jakarta / Luar Jawa', 5.0000, 'Direktur - Dirut', 0.1000, 225000.0000, 150000.0000, 0.1000, 1100000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jawa', 1.0000, 'Operator - Karu', 0.0100, 80000.0000, 60000.0000, 0.1000, 500000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jawa', 2.0000, 'Assup - Supervisor', 0.0100, 100000.0000, 80000.0000, 0.1000, 500000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jawa', 3.0000, 'Assman - Manager', 0.0100, 120000.0000, 100000.0000, 0.1000, 650000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jawa', 4.0000, 'GM - Wadir', 0.0100, 150000.0000, 150000.0000, 0.1000, 800000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jawa', 5.0000, 'Direktur - Dirut', 0.0100, 200000.0000, 150000.0000, 0.1000, 900000.0000, 'IDR');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jepang', 1.0000, 'Operator - Karu', 25.0000, 45.0000, 15.0000, 0.2000, 125.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jepang', 2.0000, 'Assup - Supervisor', 25.0000, 50.0000, 20.0000, 0.2000, 150.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jepang', 3.0000, 'Assman - Manager', 40.0000, 60.0000, 30.0000, 0.2000, 185.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jepang', 4.0000, 'GM - Wadir', 50.0000, 85.0000, 40.0000, 0.2000, 275.0000, 'USD');
+GO
+INSERT INTO [PerjalananDinas_Tarif] ([Area], [NoUrut], [Jabatan], [Transport], [UangMakan], [UangSaku], [Tiket], [Hotel], [MataUang]) VALUES ('Jepang', 5.0000, 'Direktur - Dirut', 90.0000, 135.0000, 85.0000, 0.2000, 0.0010, 'USD');
+GO
